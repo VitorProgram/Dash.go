a12e20637493fd5e3a321422d4c67968300cc858
João Vitor <joaovitornascimentoif@gmail.com>
2025-01-13 22:21:08 -0300

    build: add prisma and supabase setup

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,3 @@
-import Header from "@/components/Header";
-import Input from "@/components/Input";
-import { Flex, Stack } from "@mantine/core";
-
 const Dashboard = () => {
     return ( 
         <></>
